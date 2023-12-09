dd4971d90819fbcb991d253904474175191897a2
unknown <Tharashehadeh@gmail.com>
2023-12-09 18:31:38 +0200

    chang image

--- src/components/web/products/Products.jsx
+++ src/components/web/products/Products.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React from 'react'
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom'
-import ReactImageMagnify from 'react-image-magnify';
 import { useContext } from 'react';
 import { CartContext } from '../context/Cart.jsx';
 export default function Product() {
@@ -26,30 +25,9 @@ export default function Product() {
     <div className='container'>
         <div className='row'>
             <div className='col-lg-4 mt-3'>
-                {data.subImages.map((img,index)=> 
-                <React.Fragment key={index}>
-                <ReactImageMagnify {...{
-                    smallImage: {
-                        alt: 'Wristwatch by Ted Baker London',
-                        isFluidWidth: true,
-                        src: img.secure_url,
-                    },
-                    largeImage: {
-                        src: img.secure_url,
-                        width: 1200,
-                        height: 1800
-                    },
-                    enlargedImagePosition:'over',
-                    enlargedImageContainerDimensions:{
-                      width:900,
-                      height:900,
-                    },
-                    isHintEnabled:true,
-                }} />
-                
-                </React.Fragment>
-              )}
-            </div>
+                </div>
+            
+           
             <div className='col-lg-8'>
             <h2>{data.name}</h2>
             <p >{data.price}</p>
